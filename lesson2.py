9095eda2e25f83cd0623c48b9f3a54c8b09103cd
Nurtilekb <Nurtilekb@github.com>
2025-02-09 15:23:40 +0600

    lesson

---
+++ lesson2.py
@@ -0,0 +1,58 @@
+число=23
+текст="dcjsnciw"
+print(число, type( число))
+
+print('2 zadanie')
+a=5
+b=10
+
+число=23
+текст="dcjsnciw"
+# print(число, type( число))
+print('menayem a na b')
+print('2 zadanie')
+a=5
+b=10
+print(a)
+print(b)
+c=a
+a=b
+b=c
+print(a)
+print(b)
+x=10
+x='dcs'
+x=23.1
+x=False
+print(type(x))
+
+print('zadanie 4')
+name="Nurtilek"
+number=19
+boolin=True
+message=f"Privet name is +{name} + age{number}.chislo  Znacheni {boolin}"
+print(message)
+
+print('5 zadanie')
+a=20
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # delit chislo i tegerkteyt chykkan sandy
+print(a%b)
+
+print("6 zadanie")
+r=5
+pi=3.14
+area=pi*r*r #p*r**2 toje v kvadrat beret
+print(area)
+print(int(area)) # tut daet nam integer jana tegerkteyt
+
+print('zadabie 7') # americada temperaturany tabuu farangeight menen
+celc=18
+Farangeiht=((celc*9/5)+32)
+print(f'temperatura {Farangeiht}')
+
+print('sdcsdc')
